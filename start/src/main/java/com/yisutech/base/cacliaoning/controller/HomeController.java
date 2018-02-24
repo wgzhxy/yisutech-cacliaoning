e57b1e57e2923c72903083d7eb803e0922cf1f39
东往 <wgzhxy@163.com>
2018-02-24 17:31:01 +0800

    项目初始化

---
+++ start/src/main/java/com/yisutech/base/cacliaoning/controller/HomeController.java
@@ -0,0 +1,26 @@
+package com.yisutech.base.cacliaoning.controller;
+
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping
+public class HomeController {
+
+    @RequestMapping("/index")
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping("/print")
+    @ResponseBody
+    public Object print() {
+        JSONObject printJson = new JSONObject();
+        printJson.put("name", "李宁");
+        printJson.put("age", 35);
+        printJson.put("message", "this is a user");
+        return printJson;
+    }
+}
